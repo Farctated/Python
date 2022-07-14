ff8b42d74e2bc91704f0a465339ec5005bd50544
Farctated <abdulmomenjameli@outlook.com>
2022-07-14 22:08:45 +0100

    optimised

--- google_foobar/Find the Access Codes/solution.py
+++ google_foobar/Find the Access Codes/solution.py
@@ -1,5 +1,13 @@
-def solution(l):
-    for i in l:
-        next = i[0]
-        for j in i:
-            if 
+def solution(lst):
+    c = [0] * len(lst)
+    count = 0
+    for i in range(0,len(lst)):
+        for j in range(0, i):
+            if lst[i] % lst[j] == 0:
+                c[i] = c[i] + 1
+                count = count + c[j]
+    print(c)
+
+
+    return count
+print(solution([1,2,3,4,5,6]))
