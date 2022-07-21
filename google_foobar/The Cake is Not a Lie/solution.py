771fb866e6d578b8e9568e6aa7b2080bcf80aa82
Abdul Jameli <abdulmomenjameli@outlook.com>
2022-07-21 03:12:40 +0100

    updated solution

---
+++ google_foobar/The Cake is Not a Lie/solution.py
@@ -0,0 +1,17 @@
+def solution(s):
+    # cut slices into parts
+    # check the length is not odd
+    # make sure no remainder is left
+    # create an array/list of equal slices
+    # return the number of item in the list/slices
+    
+    l = len(s)
+    for i in range(1,l):
+        if l % i == 0:
+            equal_parts = [s[j:j+i] for j in range(0,l,i)]
+            if equal_parts[1:] == equal_parts[:-1]:
+                return len(equal_parts)
+    return 1
+
+print(solution("ababxababab"))
+
